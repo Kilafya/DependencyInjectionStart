3c0d6e181f954314bb882c4003e1b298fc621440
drayl <sunrise.161@mail.ru>
2022-06-17 17:40:05 +0300

    Add ExampleApp and Singleton for api and db

--- app/src/main/java/com/kilafyan/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
+++ app/src/main/java/com/kilafyan/dependencyinjectionstart/example2/data/network/ExampleApiService.kt
@@ -4,14 +4,19 @@ import android.content.Context
 import android.util.Log
 import com.kilafyan.dependencyinjectionstart.R
 import javax.inject.Inject
+import javax.inject.Singleton
 
+@Singleton
 class ExampleApiService @Inject constructor(
     private val context: Context,
     private val currentTime: Long
 ) {
 
     fun method() {
-        Log.d(LOG_TAG, "ExampleApiService ${context.getString(R.string.app_name)} $currentTime")
+        Log.d(
+            LOG_TAG,
+            "ExampleApiService ${context.getString(R.string.app_name)} $currentTime $this"
+        )
     }
 
     companion object {
--- app/src/main/java/com/kilafyan/dependencyinjectionstart/example2/presentation/MainActivity.kt
+++ app/src/main/java/com/kilafyan/dependencyinjectionstart/example2/presentation/MainActivity.kt
@@ -2,19 +2,18 @@ package com.example.dependencyinjectionstart.example2.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.dependencyinjectionstart.example2.data.di.DaggerApplicationComponent
 import com.kilafyan.dependencyinjectionstart.R
+import com.kilafyan.dependencyinjectionstart.example2.ExampleApp
 import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    private val component by lazy {
+        (application as ExampleApp).component
+    }
+
     @Inject
     lateinit var viewModel: ExampleViewModel
-
-    private val component by lazy {
-        DaggerApplicationComponent.factory()
-            .create(application, System.currentTimeMillis())
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         component.inject(this)
